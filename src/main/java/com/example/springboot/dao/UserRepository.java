63aafa0285e1f179ad15233e80730c149a63b9f2
ANTIMUSOR <10ner11.sc@gmail.com>
2025-03-26 03:02:43 +0300

    без лишних импортов

--- src/main/java/com/example/springboot/dao/UserRepository.java
+++ src/main/java/com/example/springboot/dao/UserRepository.java
@@ -1,11 +1,9 @@
 package com.example.springboot.dao;
 
 
-
 import com.example.springboot.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
 
 public interface UserRepository extends JpaRepository<User, Long> {
 
